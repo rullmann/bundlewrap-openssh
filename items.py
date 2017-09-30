b629c1348bbce9a19e6c8124eeead6fa4ff4b47a
Rico Ullmann <rico@erinnerungsfragmente.de>
2017-09-30 20:06:14 +0200

    proper formatting everywhere

--- items.py
+++ items.py
@@ -1,5 +1,5 @@
 pkg_dnf = {
-    "openssh-server": {},
+    'openssh-server': {},
 }
 
 svc_systemd = {
@@ -7,74 +7,74 @@ svc_systemd = {
 }
 
 files = {
-    "/etc/ssh/sshd_config": {
-        'source': "sshd_config",
-        'mode': "0600",
-        'content_type': "mako",
+    '/etc/ssh/sshd_config': {
+        'source': 'sshd_config',
+        'mode': '0600',
+        'content_type': 'mako',
         'triggers': [
-            "svc_systemd:sshd:restart",
+            'svc_systemd:sshd:restart',
         ],
     },
-    "/etc/ssh/sshd_banner": {
-        'source': "sshd_banner",
-        'mode': "0600",
-        'content_type': "mako",
+    '/etc/ssh/sshd_banner': {
+        'source': 'sshd_banner',
+        'mode': '0600',
+        'content_type': 'mako',
     },
-    "/etc/ssh/ssh_config": {
-        'source': "ssh_config",
-        'mode': "0600",
+    '/etc/ssh/ssh_config': {
+        'source': 'ssh_config',
+        'mode': '0600',
     },
 }
 
 actions = {}
 
-if node.has_bundle("firewalld"):
+if node.has_bundle('firewalld'):
     if node.metadata.get('firewalld', {}).get('default_zone'):
         default_zone = node.metadata.get('firewalld', {}).get('default_zone')
         actions['firewalld_add_ssh_zone_{}'.format(default_zone)] = {
-            'command': "firewall-cmd --permanent --zone={} --add-service=ssh".format(default_zone),
-            'unless': "firewall-cmd --zone={} --list-services | grep ssh".format(default_zone),
+            'command': 'firewall-cmd --permanent --zone={} --add-service=ssh'.format(default_zone),
+            'unless': 'firewall-cmd --zone={} --list-services | grep ssh'.format(default_zone),
             'cascade_skip': False,
             'needs': [
-                "pkg_dnf:firewalld",
+                'pkg_dnf:firewalld',
             ],
             'triggers': [
-                "action:firewalld_reload",
+                'action:firewalld_reload',
             ],
         }
     elif node.metadata.get('firewalld', {}).get('custom_zones', False):
         for interface in node.metadata['interfaces']:
             custom_zone = node.metadata.get('interfaces', {}).get(interface).get('firewalld_zone')
             actions['firewalld_add_ssh_zone_{}'.format(custom_zone)] = {
-                'command': "firewall-cmd --permanent --zone={} --add-service=ssh".format(custom_zone),
-                'unless': "firewall-cmd --zone={} --list-services | grep ssh".format(custom_zone),
+                'command': 'firewall-cmd --permanent --zone={} --add-service=ssh'.format(custom_zone),
+                'unless': 'firewall-cmd --zone={} --list-services | grep ssh'.format(custom_zone),
                 'cascade_skip': False,
                 'needs': [
-                    "pkg_dnf:firewalld",
+                    'pkg_dnf:firewalld',
                 ],
                 'triggers': [
-                    "action:firewalld_reload",
+                    'action:firewalld_reload',
                 ],
             }
     else:
         actions['firewalld_add_ssh'] = {
-            'command': "firewall-cmd --permanent --add-service=ssh",
-            'unless': "firewall-cmd --list-services | grep ssh",
+            'command': 'firewall-cmd --permanent --add-service=ssh',
+            'unless': 'firewall-cmd --list-services | grep ssh',
             'cascade_skip': False,
             'needs': [
-                "pkg_dnf:firewalld",
+                'pkg_dnf:firewalld',
             ],
             'triggers': [
-                "action:firewalld_reload",
+                'action:firewalld_reload',
             ],
         }
 
-if node.has_bundle("monit"):
+if node.has_bundle('monit'):
     files['/etc/monit.d/openssh'] = {
-        'source': "monit",
-        'mode': "0640",
-        'content_type': "mako",
+        'source': 'monit',
+        'mode': '0640',
+        'content_type': 'mako',
         'triggers': [
-            "svc_systemd:monit:restart",
+            'svc_systemd:monit:restart',
         ],
     }
